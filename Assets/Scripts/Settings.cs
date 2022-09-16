49c147bbc8bcafcade819d641da72bd546365113
HVALSTATIONEN\willi <william.z.norman@gmail.com>
2022-09-16 11:16:22 +0200

    no message

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Slider AudioSlider;
+    [SerializeField]
+    private DropdownMenu QualityLevel;
+
+    private float AudioLevel;
+
+
+    private void Start()
+    {
+        AudioSlider.maxValue = 10;
+        AudioSlider.minValue = 0;
+        AudioLevel = 5;
+        AudioSlider.value = AudioLevel;
+    }
+
+    public void Update()
+    {
+        AudioLevel = AudioSlider.value;
+
+    }
+
+
+
+}
